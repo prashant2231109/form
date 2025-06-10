7706a670bee53bbd62521e585a272c8a81da347f
prashant2231109 <prashant2231109@akgec.ac.in>
2025-06-11 02:22:08 +0530

    some changes on question api

--- index/choices.py
+++ index/choices.py
@@ -1,8 +1,8 @@
 
 QUESTION_CHOICES = (('short answer','short answer'),
                     ('long answer','long answer'),
-                    ('multiple choice ','multiple choice'),
-                    ('checkbox ','checkbox'),
+                    ('multiple choice','multiple choice'),
+                    ('checkbox','checkbox'),
                     
 
 )
